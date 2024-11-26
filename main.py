e67aa536debf5b73b04f3bcdc809f4b0c0a3d7cb
Cleyson Lima <cleysonph@gmail.com>
2024-11-26 15:23:23 -0300

    03:08 - Realizando inserção de dados com a SQL Expression Language

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-import models.imperative
+from sqlalchemy import insert
+
+from db import engine
+from models.imperative import client_table
+
+
+with engine.connect() as conn:
+    stmt = insert(client_table).values(name="Elton Fonseca", email="elton@mail.com")
+    result = conn.execute(stmt)
+    print(result.inserted_primary_key)
+    print(result.inserted_primary_key_rows)
+    print(result.last_inserted_params())
+    conn.commit()
