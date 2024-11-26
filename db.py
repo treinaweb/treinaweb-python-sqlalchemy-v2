2033e9ef3f9ba472dd1a51d766f47a2aa7932dda
Cleyson Lima <cleysonph@gmail.com>
2024-11-26 16:58:08 -0300

    04:13 - Realizando as operações de CRUD com o ORM

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 from config import config
 
@@ -6,3 +7,5 @@ from config import config
 engine = create_engine(
     f"mysql://{config.db_user}:{config.db_password}@{config.db_host}:{config.db_port}/{config.db_name}"
 )
+
+Session = sessionmaker(engine)
