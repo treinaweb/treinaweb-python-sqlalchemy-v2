64f231ef5d5088320864fad51b0ae481724c403a
Cleyson Lima <cleysonph@gmail.com>
2024-11-26 16:22:46 -0300

    03:11 - Realizando exclusão de dados com a SQL Expression Language

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-from sqlalchemy import update
+from sqlalchemy import delete
 
 from db import engine
 from models.imperative import client_table
 
 
 with engine.connect() as conn:
-    stmt = (
-        update(client_table)
-        .where(client_table.c.id == 2)
-        .values(name="Amauri Blanco", email="amauri@mail.com")
-    )
+    stmt = delete(client_table).where(client_table.c.id == 2)
     print(stmt)
     result = conn.execute(stmt)
     print(result.rowcount)
-    print(result.last_updated_params())
     conn.commit()
