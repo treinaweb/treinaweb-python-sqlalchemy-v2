c2a00c56edf9801ec02ca058ff09a3f69eee140f
Cleyson Lima <cleysonph@gmail.com>
2024-11-27 10:52:03 -0300

    05:14 - Criando o modelo Address e definindo o relacionamento com o modelo Client

--- models/declarative.py
+++ models/declarative.py
@@ -1,7 +1,8 @@
-from sqlalchemy import String
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 from db import engine
+from models.uf import UF
 
 
 class Base(DeclarativeBase): ...
@@ -14,8 +15,31 @@ class Client(Base):
     name: Mapped[str] = mapped_column(String(100))
     email: Mapped[str] = mapped_column(String(255))
 
+    address_id: Mapped[int] = mapped_column(ForeignKey("addresses.id"))
+    address: Mapped["Address"] = relationship(
+        back_populates="client", single_parent=True
+    )
+
     def __repr__(self):
         return f"<Client(id={self.id}, name={self.name}, email={self.email})>"
+
+
+class Address(Base):
+    __tablename__ = "addresses"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    state: Mapped[UF]
+    city: Mapped[str] = mapped_column(String(100))
+    neighborhood: Mapped[str] = mapped_column(String(100))
+    street: Mapped[str] = mapped_column(String(100))
+    number: Mapped[str] = mapped_column(String(10))
+
+    client: Mapped["Client"] = relationship(
+        back_populates="address", single_parent=True
+    )
+
+    def __repr__(self):
+        return f"<Address(id={self.id}, state={self.state}, city={self.city}, neighborhood={self.neighborhood}, street={self.street}, number={self.number})>"
 
 
 Base.metadata.create_all(engine)
