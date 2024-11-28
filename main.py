719a31a49d0f50198d7284e3ed1e6a8a9e0af873
Cleyson Lima <cleysonph@gmail.com>
2024-11-28 18:56:53 -0300

    05:19 - Realizando operações CRUD com relacionamentos N:N

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-import models.declarative
+from db import Session
+from models.declarative import Order, Product
+
+
+with Session() as session:
+    order = session.get(Order, 1)
+
+    order.products.append(Product(name="Product 1", price=100))
+    order.products.append(Product(name="Product 2", price=120))
+    order.products.append(Product(name="Product 3", price=200))
+
+    session.commit()
+
+
+with Session() as session:
+    order = session.get(Order, 1)
+    print(order.products)
+
+    product = session.get(Product, 1)
+    print(product.orders)
+
+
+with Session() as session:
+    order = session.get(Order, 1)
+
+    order.products.pop()
+    session.commit()
