3a6c5e4db8fa9d64470322b2b6393bebaac80901
Cleyson Lima <cleysonph@gmail.com>
2024-11-28 22:53:37 -0300

    07:21 - Explorando as estratégias de carregamento no SQLAlchemy

--- main.py
+++ main.py
@@ -1,60 +1,22 @@
-from sqlalchemy import select, func
+import logging
+from sqlalchemy import select
+from sqlalchemy.orm import joinedload, selectinload
 
 from db import Session
-from models.declarative import Client, Order, Product
+from models.declarative import Client, Order
 
-# Consulta simples
-with Session() as session:
-    # Listar todos os clientes
-    stmt = select(Client)
-    clients = session.execute(stmt).scalars().all()
-    print(clients)
+logging.basicConfig()
+logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
 
-# Consulta com filtro
-with Session() as session:
-    # Buscar cliente com id = 1
-    stmt = select(Client).where(Client.id == 1)
-    client = session.execute(stmt).scalar()
-    print(type(client))
 
-# Consulta com join
 with Session() as session:
-    # Buscar todos os pedidos de um cliente
-    stmt = select(Order).join(Order.client).where(Client.email == "alice@example.com")
-    orders = session.execute(stmt).scalars().all()
-    print(orders)
-
-# Ordenação, limitação e offset
-with Session() as session:
-    # Listar os 5 produtos mais caros, começando do terceiro
-    stmt = select(Product).order_by(Product.price.desc()).limit(5).offset(2)
-    products = session.execute(stmt).scalars().all()
-    print(products)
-
-# Consulta com agregação
-with Session() as session:
-    # Listar o total de pedidos por cliente
-    stmt = (
-        select(Client.name, func.count(Order.id))
-        .join(Order.client)
-        .group_by(Client.name)
-    )
-    result = session.execute(stmt).all()
-    for name, count in result:
-        print(f"Cliente: {name}, Total de Pedidos: {count}")
-
-# Consulta com subquery
-with Session() as session:
-    # Listar clientes que fizeram mais de um pedido
-    subquery = (
-        select(Order.client_id, func.count(Order.id).label("total_orders"))
-        .group_by(Order.client_id)
-        .subquery()
-    )
-    stmt = (
-        select(Client)
-        .join(subquery, Client.id == subquery.c.client_id)
-        .where(subquery.c.total_orders > 1)
+    stmt = select(Client).options(
+        joinedload(Client.address),
+        selectinload(Client.orders).selectinload(Order.products),
     )
     clients = session.execute(stmt).scalars().all()
-    print(clients)
+
+    for client in clients:
+        print(client, client.address, client.orders)
+        for order in client.orders:
+            print("\t", order.products)
